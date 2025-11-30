f7b4fc342318a3a20730d40d6f00fb0fe2a5f107
Arthur Beglaryan <arthur.begkaryan@gmail.com>
2025-11-30 10:44:03 +0300

    speedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "SkyWay Travel Portal",
@@ -26,6 +27,7 @@ export default async function RootLayout({
         </SessionProvider>
         <ToastContainer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
